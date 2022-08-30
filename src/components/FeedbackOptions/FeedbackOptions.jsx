1d1f49a5e23373912ef5baac34347a6a048e1ce6
Yevheniia2 <Veintq2291244@gmail.com>
2022-08-31 00:20:53 +0300

    commit_6

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,5 @@
+export function FeedbackOptions(props) {
+    return props.options.map((option) => {
+        return <button key={ option } name = { option } onClick = { props.onLeaveFeedback }>{ option }</button>
+    });
+}
